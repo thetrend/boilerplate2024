4a2315e2b2d32f79202aed0f62e0ea65f262f488
Grace de la Mora <thetrend@users.noreply.github.com>
2024-02-04 18:05:30 -0500

    revert: remove styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-import styled from 'styled-components'
-
-const StyledP = styled.p`
-  color: purple;
-`
-
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl">Hello World!</h1>
-      <StyledP>This is a paragraph</StyledP>
-    </>
-  )
+  return <h1 className="text-3xl">Hello World!</h1>
 }
 
 export default App
